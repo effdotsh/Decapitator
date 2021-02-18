4acd3cc1693a00ff2900f4ea5f71354d7e436b1e
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-02-17 20:25:16 -0500

    rewrote decapitator, init uses f-strings

--- decapitator.py
+++ decapitator.py
@@ -1,37 +1,31 @@
-import subprocess
-import requests 
+import socket
+import requests
 import json
 
-with open('data.json', 'r') as openfile: 
+with open('data.json', 'r') as openfile:
     # Reading from json file 
-    data = json.load(openfile) 
-def sendData(eth0):
-	if (data['meathod'] == 'Dweet'):
-		r = requests.post('https://dweet.io/dweet/for/' + data['access_code'] + '?eth0=' + str(eth0))
-		print(r)
-		
-		
-#read data file
-#write the output of 'ifconfig' to variable
-ifconfig = str(subprocess.check_output('ifconfig'))
-#look at each line in the output and find the ip addresses
-ifconfig = ifconfig.split('\\n')
-lineNumber = 0
-eth0_ip = False
-for line in ifconfig:
-	#ethernet
-	if(line.split(': ')[0] == 'b\'eth0' and line != ifconfig[-1]):
-		
-		row = ifconfig[lineNumber + 1]
-		#remove spaces from start of line
-		#this probably isn't required, however between different os's and systems,
-		#or by sheer bad luck the number of spaces before the line may be different,
-		#so I don't want to simply take the ninth element of the array.
-		eth0_ip = row.split('inet ')[1].split(' ')[0]
-	lineNumber += 1
-	
-if(eth0_ip != False):	
-	sendData(eth0_ip)
-	print(":)")
-else:
-	print(":(")
+    data = json.load(openfile)
+
+
+def send_data(key, info):
+    if (data['meathod'] == 'Dweet'):
+        r = requests.post(f'https://dweet.io/dweet/for/{data["access_code"]}?{key}={info}')
+        print(r)
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+
+# read the output of 'hostname -I'
+ip = get_ip()
+
+send_data('local_network', ip)
--- init.py
+++ init.py
@@ -6,61 +6,62 @@ import pathlib
 os.system('clear')
 data = open('data.json', 'w+')
 data.write('{')
+
+
 def select(choices, numOptions, question, default=1):
-	#Get inputed selection as a number
-	print(choices)
-	validSelection = False
-	while(not validSelection):
-		try:
-				selectedOption = input(question)
-				if(selectedOption == ''):
-					selectedOption = default
-				else:
-					selectedOption = int(selectedOption)
-				if(selectedOption >= 1 and selectedOption <= numOptions):
-					validSelection = True
-					return selectedOption
-		except:
-			print("Please select a valid option")
-def setupStartup():
-	#Add decapitator.py to rc.local so it runs on startup
-	print('In order for this program to do what it needs to do, it needs to run on startup')
-	approval = input('Would you like this to do that automatically? (y/n): ')
-	while approval.lower() not in {'y', 'n', 'yes', 'no'}:
-		approval = input('Would you like this to do that automatically? (y/n): ')
-	if approval.lower()[0] =='y':
-		rc = open('/etc/rc.local', 'a')
-		check_rc = open('/etc/rc.local', 'r')
-		shouldWrite = True
-		for line in check_rc:
-			if 'python3 decapitator.py ' in line:
-				shouldWrite = False
-				print('It looks like this program is already in your startup. It won\'t be added again')
-		if (shouldWrite):
-			print(pathlib.Path(__file__).parent.absolute())
-			rc.write('cd ' + str(pathlib.Path(__file__).parent.absolute()) + '\n')
-			rc.write('python3 decapitator.py \n')
-			rc.write('cd \n')
-			rc.flush()
-			rc.close()
+    # Get inputed selection as a number
+    print(choices)
+    validSelection = False
+    while (not validSelection):
+        try:
+            selectedOption = input(question)
+            if (selectedOption == ''):
+                selectedOption = default
+            else:
+                selectedOption = int(selectedOption)
+            if (selectedOption >= 1 and selectedOption <= numOptions):
+                validSelection = True
+                return selectedOption
+        except:
+            print("Please select a valid option")
 
 
+def setup_startup():
+    # Add decapitator.py to rc.local so it runs on startup
+    print('In order for this program to do what it needs to do, it needs to run on startup')
+    approval = input('Would you like this to do that automatically? (y/n): ')
+    while approval.lower() not in {'y', 'n', 'yes', 'no'}:
+        approval = input('Would you like this to do that automatically? (y/n): ')
+    if approval.lower()[0] == 'y':
+        rc = open('/etc/rc.local', 'a')
+        check_rc = open('/etc/rc.local', 'r')
+        shouldWrite = True
+        for line in check_rc:
+            if 'python3 decapitator.py ' in line:
+                shouldWrite = False
+                print('It looks like this program is already in your startup. It won\'t be added again')
+        if (shouldWrite):
+            print(pathlib.Path(__file__).parent.absolute())
+            rc.write(f'cd {str(pathlib.Path(__file__).parent.absolute())}\n')
+            rc.write('python3 decapitator.py \n')
+            rc.write('cd \n')
+            rc.flush()
+            rc.close()
 
 
-
-
-options = '''
-Thank you for installing Decapitator, a simple tool that allows you not to worry about changing non-static IP addresses. Please select a method of notification (currently I have only set up one, more will hopefully come in the future):
+options = '''Thank you for installing Decapitator, a simple tool that allows you not to worry about changing 
+non-static IP addresses. Please select a method of notification (currently I have only set up one, more will 
+hopefully come in the future): 
 
 1. Dweet - A simple, free, IoT messaging system.
 
 '''
 selectedOption = select(options, 1, "Choose a meathod (1): ")
 
-#dweet
+# dweet
 if selectedOption == 1:
-	meathod = "Dweet"
-	options = '''
+    meathod = "Dweet"
+    options = '''
 	You have chosen Dweet. There are some Dweet customizations to choose from.
 	This code only supports publicly accessible dweets. This means that if anyone has
 	your access code, they can get your IP. This means that having a random access code
@@ -74,23 +75,23 @@ if selectedOption == 1:
 	1. Random
 	2. Custom
 	'''
-	rand_or_custom = select(options, 2, "Would you like a random or custom access code (1): ")
-	data.write('"rand_or_custom":' + str(rand_or_custom) + ',')
-	access_code = (''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(15))).lower()
-	if(rand_or_custom == 2):
-		access_code = input("Make a unique access code: ")
-	data.write('"access_code":"'+access_code+'",')
-	setupStartup()
-	print('\n')
-	print("That's it! Thank you for using decapitator, the best headless solution. You can now access your pi's ip at the following link. This will update every time the pi reboots: ")
-	print("https://dweet.io/get/latest/dweet/for/"+access_code)
-data.write('"meathod":"' + meathod+'"')
+    rand_or_custom = select(options, 2, "Would you like a random or custom access code (1): ")
+    data.write(f'"rand_or_custom": {str(rand_or_custom)},')
+    access_code = (
+        ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(15))).lower()
+    if (rand_or_custom == 2):
+        access_code = input("Make a unique access code: ")
+    data.write('"access_code":"' + access_code + '",')
+    setup_startup()
+    print('\n')
+    print(
+        "That's it! Thank you for using decapitator, the best headless solution. You can now access your pi's ip at "
+        "the following link. This will update every time the pi reboots: ")
+    print(f'https://dweet.io/get/latest/dweet/for/{access_code}')
+data.write(f'"meathod": "{meathod}"')
 data.write('}')
 data.flush()
 data.close()
+
+print('done')
 os.system("python3 decapitator.py")
-
-
-
-
-
